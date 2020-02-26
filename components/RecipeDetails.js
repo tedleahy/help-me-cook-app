230f432cc0f7705507b4135d03bf7ee5855fdd69
Ted Leahy <edwardleahy@protonmail.com>
2020-02-26 10:29:47 +0000

    Display recipe instructions on RecipeDetails screen

--- components/RecipeDetails.js
+++ components/RecipeDetails.js
@@ -1,26 +1,30 @@
 // Viewing the details of a single recipe
 
 import React from 'react'
-import { Text, StyleSheet, View, FlatList } from 'react-native'
-import Constants from 'expo-constants'
+import { Text, StyleSheet, ScrollView, FlatList } from 'react-native'
 
 export default function RecipeDetails({route}) {
   const recipe = route.params.recipe.attributes
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        {recipe.name}
-      </Text>
-      <Text style={styles.ingredientsHeader}>
-        Ingredients
-      </Text>
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>{recipe.name}</Text>
+
+      <Text style={styles.sectionHeader}>Ingredients</Text>
       <FlatList
         data={recipe.ingredients}
         renderItem={({item}) => displayIngredient(item)}
         keyExtractor={item => item.name}
       />
-    </View>
+
+      <Text style={styles.sectionHeader}>Instructions</Text>
+      <FlatList
+        style={{ marginBottom: 20 }}
+        data={recipe.instructions}
+        renderItem={({item, index}) => <Text>{index+1}. {item}</Text>}
+        keyExtractor={(_, index) => `${index}`}
+      />
+    </ScrollView>
   )
 }
 
@@ -28,19 +32,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    padding: 10,
-    paddingTop: Constants.statusBarHeight
+    paddingHorizontal: 10,
   },
   title: {
     fontSize: 30,
     fontWeight: '300',
-    marginBottom: 20,
-    alignSelf: 'center'
+    alignSelf: 'center',
+    marginTop: 10
   },
-  ingredientsHeader: {
+  sectionHeader: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 5
+    marginBottom: 5,
+    marginTop: 10
   }
 })
 
